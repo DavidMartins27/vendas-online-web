a91f513e0e270cc3a2e8d6a495bf1871ce251c9b
DavidMartins27 <135028080+DavidMartins27@users.noreply.github.com>
2025-06-09 15:24:35 -0300

    feat/ Create Login Component
    criação do componente de login

--- src/modules/login/styles/loginScreen.styles.ts
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
+export const ContainerLoginScreen = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: right;
+`;
+
 export const BackGroundImage = styled.img`
     position: absolute;
     top: 0;
@@ -7,4 +13,27 @@ export const BackGroundImage = styled.img`
     width: 100%;
     height: 100vh;
     object-fit: cover;
-`;  
+    z-index: -1;
+`;
+
+export const LogoImage = styled.img`
+`;
+
+export const ContainerLogin = styled.div`
+    background-color: #D9D9D9;
+    width: 100%;
+    max-width: 646px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 22px;
+`;
+
+export const LimitedContainer = styled.div`
+    width: 100%;
+    max-width: 498px;
+    display: flex;
+    justify-content: center;
+`;
